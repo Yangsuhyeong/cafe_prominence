afcb59c33ee5cb4083be0214090246f97922c2e3
Yangsuhyung <sara2601@naver.com>
2018-02-20 17:49:31 +0900

    111

--- google_cafe.py
+++ google_cafe.py
@@ -12,9 +12,7 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 MAX_FETCH = 200
 AUTH_KEY = "AIzaSyCPrWFxTwfbV1LJSMPyEnroNh_Ezb2KEhI"
 
-#url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?&key=" + AUTH_KEY + "&location"
 google_places = GooglePlaces(AUTH_KEY)
-#google_places = GooglePlaces(AUTH_KEY)
 query_result = google_places.nearby_search(
 	location = 'Seoul, Korea', keyword = 'cafe', rankby = 'prominence', type = 'cafe', radius = 50000)
 #text_search API
@@ -38,11 +36,11 @@ while fetched < MAX_FETCH :
 		vic2 = vic.replace(","," ")
 		lat = str(place.details['geometry']['location']['lat'])
 		lng = str(place.details['geometry']['location']['lng'])
-#		print('[%i] %s'%(fetched, name))
-#		print(rating)
+		print('[%i] %s'%(fetched, name))
+		print(rating)
 		print(vic2)
-#		print(lat)
-#		print(lng)
+		print(lat)
+		print(lng)
 		fetched = fetched + 1
 		row = name + "," + rating + "," + vic2 + "," + lat + "," + lng + "\n"
 		dataset2.write(row)
@@ -53,16 +51,6 @@ while fetched < MAX_FETCH :
 		print('=========================================')
 		query_result = google_places.nearby_search(
 			pagetoken = query_result.next_page_token)
-#		row = name + "," + rating + "," + vic + "," + lat + "," + lng + "\n"
-#		dataset2.write(row)
-
-#for place in query_result.places:
-#	print(place.name)
-#	print(place.geo_location)
-#	print(place.place_id)
-#	place.get_details()
-#	print(place.details)
-#	print(place.url) #local_phone_number, website,
 
 #for photo in place.photos :
 #	photo.get(maxheight=500, maxwidth=500)
@@ -70,8 +58,3 @@ while fetched < MAX_FETCH :
 #	photo.url
 #	photo.filename
 #	photo.data
-
-#additional pages of results?
-#if query_result.has_next_page_token:
-#	query_result_next_page = google_places.nearby_search(
-#		pagetoken = query_result.next_page_token)
