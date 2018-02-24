0de9ffe4b82ef2252a1188274923d2329c37c470
Yangsuhyung <sara2601@naver.com>
2018-02-24 23:24:38 +0900

    ook

--- visualization.py
+++ visualization.py
@@ -1,7 +1,18 @@
-import urllib
+import csv
+from bs4 import BeautifulSoup
+#import folium
 import os
-import sys
 import io
+import sys
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
-import folium
+
+reader = csv.reader(open('google_cafe_dataset.csv','r'), delimiter = ",")
+
+for line in reader:
+    print(line[0])
+#    print(line[2])
+    a = line[2]
+    b = a.replace("서초구", "Seocho-gu")
+    c = b.split('  ')
+    print(c)
